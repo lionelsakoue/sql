c35baa00a6916effb30cf844318651e75c18b895
lionelsakoue <130628424+lionelsakoue@users.noreply.github.com>
2024-09-03 22:36:10 -0400

    Update output.tf

--- sql-terraform/output.tf
+++ sql-terraform/output.tf
@@ -1,10 +1,9 @@
 output "instance_name" {
   description = "The name of the created SQL instance."
-  value       = google_sql_database_instance.my_sql_instance.name
+  value       = google_sql_database_instance.main.name
 }
 
 output "instance_region" {
   description = "The region where the SQL instance is located."
-  value       = google_sql_database_instance.my_sql_instance.region
+  value       = google_sql_database_instance.main.region
 }
-
