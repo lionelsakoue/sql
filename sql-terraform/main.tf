bb5c0c1070309d6470fc22960c49161dfc34e266
lionelsakoue <130628424+lionelsakoue@users.noreply.github.com>
2024-09-03 21:53:30 -0400

    Update main.tf

--- sql-terraform/main.tf
+++ sql-terraform/main.tf
@@ -2,9 +2,10 @@ resource "google_sql_database_instance" "my_sql_instance" {
   name             = var.instance_name
   database_version = var.database_version
   region           = var.region
-  tier             = var.tier
 
-  root_password {
-    password = var.root_password
+  settings {
+    tier = var.tier
   }
+
+  root_password = var.root_password
 }
